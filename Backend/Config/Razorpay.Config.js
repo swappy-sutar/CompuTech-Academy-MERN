ee3000535b84811eb487b3c3c15ceb1cfdddeb46
swapnil <sutarswapnil322@gmail.com>
2024-11-12 21:49:14 +0530

    create configration cloudinary and Razorpay

--- Backend/Config/Razorpay.Config.js
+++ Backend/Config/Razorpay.Config.js
@@ -1,9 +1,10 @@
 import Razorpay from "razorpay";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
-})
+  key_id: process.env.RAZORPAY_KEY,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
 
-export {instance}
+export {instance};
