7e3fda958aca16d9d668a7c9fc85cff51b5c250d
swapnil <sutarswapnil322@gmail.com>
2024-11-15 00:48:46 +0530

    create TimelineSection section and add to home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,7 +5,8 @@ import HighlightText from "../Components/core/HomePage/HighlightText";
 import CTAButton from "../Components/core/HomePage/CTAButton";
 import Banner from "../assets/Images/banner.mp4";
 import CodeBlock from "../Components/core/HomePage/CodeBlock";
-
+import LearningLanguageSection from "../Components/core/HomePage/LearningLanguageSection";
+import TimelineSection from "../Components/core/HomePage/TimelineSection";
 function Home() {
   return (
     <div>
@@ -97,12 +98,57 @@ function Home() {
               active: false,
             }}
             CodeBlock={`<!DOCTYPE html>\n <head>\n <title>Compu-Tech</title>\n</head>\n<body>\n <div>\n  Hello Wrold !!\n </div>\n</body>\n</html>`}
-            codeColor={"text-yellow-25"}
+            codeColor={"text-blue-25"}
           />
         </div>
       </div>
 
       {/* Section 2 */}
+      <div className="bg-pure-greys-5 text-richblack-700 ">
+        <div className="homepage_bg h-[310px] ">
+          <div className="w-11/12 flex-col max-w-maxContent flex justify-center items-center gap-5 mx-auto ">
+            <div className="h-[150px] "></div>
+
+            <div className="flex flex-row gap-7 text-white">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex gap-2 items-center">
+                  Explore Full Catelog
+                  <FaLongArrowAltRight />
+                </div>
+              </CTAButton>
+
+              <CTAButton active={false} linkto={"/signup"}>
+                <div>Learn More</div>
+              </CTAButton>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7">
+          <div className="flex flex-row gap-5 mb-10 mt-[95px]">
+            <div className="text-4xl font-semibold w-[45%]">
+              Get the Skills you need for a
+              <HighlightText text={"Job that is in demand"} />
+            </div>
+
+            <div className="flex flex-col gap-10 w-[40%] items-start">
+              <div className="text-[16px]">
+                The modern StudyNotion is the dictates its own terms. Today, to
+                be a competitive specialist requires more than professional
+                skills.
+              </div>
+              <CTAButton active={true} linkto={"/signup"}>
+                <div>Learn more</div>
+              </CTAButton>
+            </div>
+          </div>
+
+          <TimelineSection />
+
+          <LearningLanguageSection />
+        </div>
+      </div>
+
       {/* Section 3 */}
       {/* Section 4 */}
       {/* Footer */}
