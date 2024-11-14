2eb61e1f737a14fefee51948b9d9e6950f18c98b
swapnil <sutarswapnil322@gmail.com>
2024-11-14 19:36:51 +0530

    create components CodeBlock, CTAButton, HighlightText

---
+++ Frontend/src/Components/core/HomePage/HighlightText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HighlightText({text}) {
+  return (
+   <span className='font-bold text-blue-50'>
+    {" "}
+    {text}
+   </span>
+  )
+}
+
+export default HighlightText
