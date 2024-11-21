ef7f0e72ec23f4d542148967cc94a3b1e0343f13
swapnil <sutarswapnil322@gmail.com>
2024-11-21 20:37:10 +0530

    add routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,66 @@ import {Route, Routes} from "react-router-dom"
 import './App.css'
 import Home from "./Pages/Home";
 import Navbar from "./Components/common/Navbar";
+import Signup from "./Pages/Signup"
+import Login from "./Pages/Login"
+import ForgotPassword from "./Pages/ForgotPassword";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import UpdatePassword from "./Pages/UpdatePassword";
+import VerifyEmail from "./Pages/VerifyEmail";
+import Error from "./Pages/Error";
+import About from "./Pages/About";
+
 
 function App() {
 
   return (
-    <div className="w-screen min-h-screen   bg-richblack-900 flex flex-col font-inter">
-      <Navbar/>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="error" element={<Error />} />
+        <Route path="about" element={<About />} />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+        <Route path="error" element={<Error />} />
       </Routes>
     </div>
   );
