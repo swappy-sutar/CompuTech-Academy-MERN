fcd85ceb21e41ff06c585e452d9982fd7db6fe24
swapnil <sutarswapnil322@gmail.com>
2024-11-21 20:36:39 +0530

    add toster

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./Reducer";
+import { Toaster } from "react-hot-toast";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
