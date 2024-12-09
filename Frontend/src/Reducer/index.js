8c435937f764831fd2bcf18f1ef6ad474b45698e
swapnil <sutarswapnil322@gmail.com>
2024-12-10 01:02:07 +0530

    add cartReducer and courseReducer in combineReducers

--- Frontend/src/Reducer/index.js
+++ Frontend/src/Reducer/index.js
@@ -2,12 +2,14 @@ import { combineReducers} from "@reduxjs/toolkit"
 import authReducer from "../Slices/Auth.slice.js"
 import profileReducer from "../Slices/Profile.slice.js"
 import cartReducer from "../Slices/Cart.Slice.js"
+import courseReducer from "../Slices/Course.slice.js"
 
 
 const rootReducer = combineReducers({
     auth:authReducer,
     profile:profileReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    course:courseReducer,
 
 
 })
