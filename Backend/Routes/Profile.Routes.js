62e7312c29e70b1fca40fab08489d067cb1574ee
swapnil <sutarswapnil322@gmail.com>
2024-12-10 00:07:03 +0530

    create route for updateUserName

--- Backend/Routes/Profile.Routes.js
+++ Backend/Routes/Profile.Routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { auth } from "../Middlewares/Auth.Middleware.js";
 import {
+  updateUserName,
   updateProfile,
   deleteAccount,
   getAllUsers,
@@ -10,6 +11,8 @@ import {
 
 const router = express.Router();
 router.post("/update-profile", auth, updateProfile);
+router.post("/update-name", auth, updateUserName);
+
 router.get("/get-users-details", auth, getAllUsers);
 router.delete("/delete-account", auth, deleteAccount);
 
