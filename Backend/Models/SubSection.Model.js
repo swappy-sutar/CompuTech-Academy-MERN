958de71c8a9b42223c8a7c4956b445c6b001fb18
swapnil <sutarswapnil322@gmail.com>
2024-12-10 00:05:08 +0530

    fix the typo of description

--- Backend/Models/SubSection.Model.js
+++ Backend/Models/SubSection.Model.js
@@ -1,19 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
 const subSectionSchema = new Schema({
-    title:{
-        type:String,
-    },
-    desciption:{
-        type:String
-    },
-    timeDuration:{
-        type:String,
-    },
-    videoUrl:{
-        type:String,
-    }
-     
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  timeDuration: {
+    type: String,
+  },
+  videoUrl: {
+    type: String,
+  },
 });
 
 export const SubSection = mongoose.model("SubSection", subSectionSchema);
