90ec3521629012a7a6f65d67dbc723ed38538829
swapnil <sutarswapnil322@gmail.com>
2024-12-10 00:48:02 +0530

    set cookies when user is login and chnage the messages in catch part

--- Frontend/src/Services/operations/authAPI.js
+++ Frontend/src/Services/operations/authAPI.js
@@ -5,6 +5,7 @@ import { resetCart } from "../../slices/Cart.Slice.js";
 import { setUser } from "../../slices//Profile.slice.js";
 import { apiConnector } from "../apiconnector";
 import { endpoints } from "../api";
+import Cookies from "js-cookie";
 
 const {
   SENDOTP_API,
@@ -16,7 +17,6 @@ const {
 
 export function sendOtp(email, navigate) {
   return async (dispatch) => {
-    // Show loading toast
     const toastId = toast.loading("Sending OTP...");
     dispatch(setLoading(true));
 
@@ -42,9 +42,11 @@ export function sendOtp(email, navigate) {
     } catch (error) {
       console.error("SENDOTP API ERROR............", error);
 
-      const errorMessage = error.response?.data?.message || "Could not send OTP. Please try again.";
+      const errorMessage =
+        error.response?.data?.message ||
+        "An error occurred while send OTP. Please try again.";
       console.log("errorMessage", errorMessage);
-      
+
       toast.error(errorMessage);
     } finally {
       dispatch(setLoading(false));
@@ -77,28 +79,26 @@ export function signUp(
         confirmPassword: confirmPassword,
         otp: otp,
       });
-   
-      
+
       console.log("SIGNUP API RESPONSE............", response);
 
-
-      toast.success(response.data.message);
+      toast.success("User created");
       navigate("/login");
-
     } catch (error) {
       console.error("SIGNUP API ERROR............", error);
 
-      const errorMessage = error.response?.data?.message || "Unable to create account. Please try again.";
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An error occurred while creating account. Please try again.";
 
       toast.error(errorMessage);
-      
     } finally {
       dispatch(setLoading(false));
       toast.dismiss(toastId);
     }
   };
 }
-
 
 export function login(email, password, navigate) {
   return async (dispatch) => {
@@ -117,18 +117,23 @@ export function login(email, password, navigate) {
       }
 
       toast.success("Login Successful");
+
       dispatch(setToken(response.data.token));
       const userImage = response.data?.user?.image
         ? response.data.user.image
         : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`;
       dispatch(setUser({ ...response.data.user, image: userImage }));
-
-      localStorage.setItem("token", JSON.stringify(response.data.token));
+      
+      Cookies.set("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.user));
       navigate("/dashboard/my-profile");
     } catch (error) {
       console.log("LOGIN API ERROR............", error);
-      toast.error("Login Failed");
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An error occurred while login"; 
+      toast.error(errorMessage);
     }
     dispatch(setLoading(false));
     toast.dismiss(toastId);
@@ -140,7 +145,7 @@ export function logout(navigate) {
     dispatch(setToken(null));
     dispatch(setUser(null));
     dispatch(resetCart());
-    localStorage.removeItem("token");
+    Cookies.remove("token");
     localStorage.removeItem("user");
     toast.success("Logged Out");
     navigate("/");
@@ -149,12 +154,10 @@ export function logout(navigate) {
 
 export function getPasswordResetToken(email, setEmailSent) {
   return async (dispatch) => {
-
     dispatch(setLoading(true));
     const toastId = toast.loading("Sending reset email...");
 
     try {
-
       const response = await apiConnector("POST", RESETPASSTOKEN_API, {
         email,
       });
@@ -162,7 +165,8 @@ export function getPasswordResetToken(email, setEmailSent) {
       console.log("RESET PASSWORD TOKEN RESPONSE....", response);
 
       if (!response.data.success) {
-        const errorMessage = response.data.message || "Failed to send reset email";
+        const errorMessage =
+          response.data.message || "Failed to send reset email";
         console.log("Error message from API:", errorMessage);
 
         toast.error(errorMessage, { id: toastId });
@@ -175,14 +179,16 @@ export function getPasswordResetToken(email, setEmailSent) {
     } catch (error) {
       console.error("RESET PASSWORD TOKEN Error:", error);
 
-      const errorMessage = error.response?.data?.message || "Failed to send email for resetting password";
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An error occurred while send email for resetting password";
       toast.error(errorMessage, { id: toastId });
     } finally {
       dispatch(setLoading(false));
     }
   };
 }
-
 
 export function resetPassword(password, confirmPassword, token) {
   return async (dispatch) => {
@@ -203,7 +209,11 @@ export function resetPassword(password, confirmPassword, token) {
       toast.success("Password has been reset successfully");
     } catch (error) {
       console.log("RESET PASSWORD TOKEN Error", error);
-      toast.error("Unable to reset password");
+       const errorMessage =
+         error.response?.data?.message ||
+         error.message ||
+         "An error occurred while Reset password token";
+       toast.error(errorMessage);
     }
     dispatch(setLoading(false));
   };
